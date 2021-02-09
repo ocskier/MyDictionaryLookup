3c4c090be782d87b19682b7bf316e1ac3df67b9d
Jon Jackson <ocskier@gmail.com>
2021-02-09 16:42:59 -0500

    feat: add word search widget function to return definition

--- dbapp.py
+++ dbapp.py
@@ -3,19 +3,20 @@ from tkinter.constants import END
 from mysql.connector import errorcode
 from db.connection import connection
 
-cursor = connection.cursor()
-queryTerm = "'freon'"
-query = """SELECT * FROM Dictionary WHERE Expression={queryTerm}""".format(queryTerm=queryTerm)
-try: 
-    data = cursor.execute(query)
-    for row in cursor:
-        print(json.dumps(row, indent=2))
-except mysql.connector.Error as err:
-    print(err)
-
 window = tkinter.Tk()
 
 e1Val = tkinter.StringVar()
+
+def runSearch():
+    cursor = connection.cursor()
+    queryTerm = """'{input}'""".format(input=e1Val.get())
+    query = """SELECT * FROM Dictionary WHERE Expression={queryTerm}""".format(queryTerm=queryTerm)
+    try: 
+        data = cursor.execute(query)
+        for row in cursor:
+            t1.insert('1.0', row[1])
+    except mysql.connector.Error as err:
+        print(err)
 
 e1 = tkinter.Entry(window, textvariable=e1Val)
 e1.grid(row=0,column=0)
@@ -23,7 +24,7 @@ e1.grid(row=0,column=0)
 t1 = tkinter.Text(window, height=1, width=20)
 t1.grid(row=0,column=1)
 
-b1 = tkinter.Button(window, text="Search")
+b1 = tkinter.Button(window, text="Search",command=runSearch)
 b1.grid(row=0,column=2)
 
 window.mainloop()
