6a3cf11c86bff5e2d0bef4f430a56a26895eed17
Jon Jackson <ocskier@gmail.com>
2021-02-10 23:47:47 -0500

    feat: add scrolltextbox, clear entry and text on submit

--- dbapp.py
+++ dbapp.py
@@ -1,5 +1,6 @@
 import tkinter, json, mysql.connector
 from tkinter.constants import END
+from tkinter.scrolledtext import ScrolledText
 from mysql.connector import errorcode
 from db.connection import connection
 
@@ -10,22 +11,28 @@ e1Val = tkinter.StringVar()
 def runSearch():
     cursor = connection.cursor()
     queryTerm = """'{input}'""".format(input=e1Val.get())
+    print(queryTerm)
     query = """SELECT * FROM Dictionary WHERE Expression={queryTerm}""".format(queryTerm=queryTerm)
+    e1.delete(0,END)
+    s1.delete('1.0',END)
     try: 
         data = cursor.execute(query)
         for row in cursor:
-            t1.insert('1.0', row[1])
+            s1.insert('1.0', row[1])
     except mysql.connector.Error as err:
         print(err)
 
-e1 = tkinter.Entry(window, textvariable=e1Val)
-e1.grid(row=0,column=0)
+l1 = tkinter.Label(window, text='Word: ')
+l1.grid(row=0,column=0)
 
-t1 = tkinter.Text(window, height=1, width=20)
-t1.grid(row=0,column=1)
+e1 = tkinter.Entry(window, textvariable=e1Val, width=30)
+e1.grid(row=0,column=1)
 
-b1 = tkinter.Button(window, text="Search",command=runSearch)
+b1 = tkinter.Button(window, text="Search",command=runSearch, width=20)
 b1.grid(row=0,column=2)
+
+s1 = ScrolledText(window, width=50, height=5)
+s1.grid(row=1,column=0)
 
 window.mainloop()
 
